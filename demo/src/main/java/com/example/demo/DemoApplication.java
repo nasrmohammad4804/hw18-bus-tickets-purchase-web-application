b237fdf12d9d3bbc97bba439b449068e1c68c58b
mohammad nasr <nasrmohammad4804@gmail.com>
2021-10-26 10:53:21 +0330

    configuration file and main file

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import com.example.demo.domain.Ticket;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+/*	@Bean
+	@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public Ticket getTicket(){
+		return new Ticket();
+	}*/
+}
